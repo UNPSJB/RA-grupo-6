919cafd45b133959d5650e64a50ec3e83256f8ef
Macarena <pereiramacarena394@gmail.com>
2025-10-23 17:08:47 -0300

    Correccion tiempo modal de error

--- Frontend/src/components/Formulario/PlantillaFormularioCreate.tsx
+++ Frontend/src/components/Formulario/PlantillaFormularioCreate.tsx
@@ -20,7 +20,7 @@ function CrearPlantillaFormulario() {
     setMostrarError(true);
     setTimeout(() => {
       setMostrarError(false);
-    }, 3000); 
+    }, 1000); 
   };
 
   const validarFormulario = () => {

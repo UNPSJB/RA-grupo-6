89992e8dcc3a219608aed55953d3728dce9fda2b
Macarena <pereiramacarena394@gmail.com>
2025-11-24 18:59:08 -0300

    cambio de logo de user

--- Frontend/src/components/coreui-components/header/AppHeaderDropdown.tsx
+++ Frontend/src/components/coreui-components/header/AppHeaderDropdown.tsx
@@ -39,21 +39,13 @@ const AppHeaderDropdown = () => {
   return (
     <CDropdown variant="nav-item" placement="bottom-end">
       <CDropdownToggle className="py-0 pe-0" caret={false}>
-        <CAvatar src={avatar8} size="md" />
+        <i className="fa-solid fa-user mt-1" style={{fontSize:"28px"}}></i>
       </CDropdownToggle>
       <CDropdownMenu className="pt-0">
         <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">
           {user ? `${user.nombre} ${user.apellido}` : 'Cuenta'}
           <div className="small text-medium-emphasis">{user?.rol?.nombre}</div>
         </CDropdownHeader>
-        <CDropdownItem href="#">
-          <CIcon icon={cilUser} className="me-2" />
-          Perfil
-        </CDropdownItem>
-        <CDropdownItem href="#">
-          <CIcon icon={cilSettings} className="me-2" />
-          Configuración
-        </CDropdownItem>
         <CDropdownDivider />
         <CDropdownItem onClick={handleLogout} style={{ cursor: 'pointer' }}>
           <CIcon icon={cilAccountLogout} className="me-2" />
